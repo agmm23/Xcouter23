77b11717762faf185dcde3b2118167e9efdcf760
Alfonso <agmm23@gmail.com>
2021-04-25 22:32:49 +0000

    test

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 from flask import Flask
 import sqlalchemy as sql
 import pandas as pd
-import os.path
-import sys
-import configparser
+
 
 app = Flask(__name__)
 
-absPath = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), 'config.ini')
-
-config = configparser.ConfigParser()
-config.read(absPath)
-
-host = config['MySQL.Info']['host']
-usr = config['MySQL.Info']['user']
-usrPass = config['MySQL.Info']['password']
-db = config['MySQL.Info']['database']
-
-
-connect_string = 'mysql+mysqlconnector://{usr}:{usrPass}@{host}/{db}'.format(usr=usr, usrPass=usrPass, host=host, db=db)
+connect_string = 'mysql+mysqlconnector://agmm23:mysqlpassword1a@agmm23.mysql.pythonanywhere-services.com/agmm23$xcouter_db'
+#connect_string = 'mysql+mysqlconnector://root:password@localhost/xcouter_lub_fem_2020'
 
 sql_engine = sql.create_engine(connect_string)
 query = "select * from playbyplay limit 1" #Todo Cambiar la query por una de alchemy
